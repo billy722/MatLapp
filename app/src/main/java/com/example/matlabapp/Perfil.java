d4026b884c3cf34a4ea0ff929b24c72a6ef0487a
billy722 <bill_722@hotmail.com>
2019-03-28 16:07:29 -0300

    AGRGA Y PROGRAMA SPINNER CURSO

--- app/src/main/java/com/example/matlabapp/Perfil.java
+++ app/src/main/java/com/example/matlabapp/Perfil.java
@@ -2,12 +2,23 @@ package com.example.matlabapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Perfil extends AppCompatActivity {
+    Spinner selector_cursos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+
+        selector_cursos = findViewById(R.id.select_curso);
+
+        String [] cursos = {"Primero","Segundo","Tercero","Cuarto","Quinto","Sexto","Septimo","Octavo"};
+
+        ArrayAdapter <String> lista_cursos = new ArrayAdapter<String>( this, R.layout.estilo_spinner,cursos);
+        selector_cursos.setAdapter(lista_cursos);
     }
 }
